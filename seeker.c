469dd57f2786f7a3c8ece1579bf30ce848b08eb3
Gustavo Romero <gromero@br.ibm.com>
2016-10-09 14:22:00 -0300

    First funcional jited instruction seeker
    First functional jited instruction seeker able to find some
    valid and invalid instruction on x64. It uses fork and at some
    point it halts. Halting cause is unknown as of now. Currently it
    just seeks for 4 bytes instructions, thus a refinement is needed.

--- seeker.c
+++ seeker.c
@@ -1,27 +1,73 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/mman.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+void codeCache(unsigned int instr)
+{
+ //asm(".long 0xBABEBEEF");
+ //asm("nop");
+ //int data[] = "0xBABEBEEF"
+
+ //unsigned char data[] = {0xB8, 0x00, 0x00, 0x00, 0x00, 0xC3};
+ unsigned char data[] = {0x90, 0x90, 0x90, 0x90, 0xC3};
+ data[0] = instr;
+ int (*f)(void);
+ void *mem;
+
+ mem = mmap(NULL, 5 /* 6 bytes */, PROT_WRITE | PROT_EXEC, MAP_ANON | MAP_PRIVATE, -1, 0);
+ memcpy(mem,data, 5 );
+
+#if DEBUG
+ printf("0x%x\n", data);
+#endif
+
+ f = mem ;
+ f();
+}
 
 int main(void)
 {
  int pid;
  int cstatus;
 
+ unsigned int instruction = 0x00000000;
+
+spawn:
+ if ( !(instruction <= 0xFFFFFF))
+  exit(0);
+
  pid = fork();
 
  if (pid) {
-  printf("I'm the parent\n");
-  printf("PPID %d\n", getpid());
+//printf("I'm the parent\n");
+//printf("PPID %d\n", getpid());
   waitpid(pid, &cstatus, 0);
-  if (WIFEXITED(cstatus)) {
-    printf("Child exited fine\n");
-  } else {
-    printf("Something went wrong to the child. Exited not normally.\n");
+
+  // Skip halting instruction below:
+  if (instruction == 0x4f ||
+      instruction == 0x4e   ) {
+   instruction++;
+   goto spawn;
   }
+
+  if (WIFEXITED(cstatus)) {
+    printf("Instruction: 0x%x is valid.\n", instruction);
+  //printf("Child exited fine\n");
+  } else {
+    printf("Instruction: 0x%x is invalid.\n", instruction);
+  //printf("Something went wrong to the child %d. Exited not normally.\n", pid);
+  }
+
+  instruction++;
+  goto spawn;
+
  } else {
-  asm(".long  0xBABEBEEF");
-  printf("I'm the child\n");
-  printf("CPID: %d\n", getpid());
+  codeCache(instruction);
+//printf("I'm the child\n");
+//printf("CPID: %d\n", getpid());
  }
 }
