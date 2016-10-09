7b1280cd76b6374f14be8d6c169f434635d8f200
Gustavo Romero <gromero@br.ibm.com>
2016-10-09 11:00:26 -0300

    An example on how to verify how child dies
    Child will execute the jited code. Sometimes it will crash as a result
    of an illegal instruction or load/store that yields a segfault. waitpid()
    will grab how child died so the parent can decide what to due. If child
    exit normally it means the jited instruction is valid.

--- seeker.c
+++ seeker.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(void)
 {
  int pid;
+ int cstatus;
 
  pid = fork();
 
  if (pid) {
   printf("I'm the parent\n");
   printf("PPID %d\n", getpid());
+  waitpid(pid, &cstatus, 0);
+  if (WIFEXITED(cstatus)) {
+    printf("Child exited fine\n");
+  } else {
+    printf("Something went wrong to the child. Exited not normally.\n");
+  }
  } else {
+  asm(".long  0xBABEBEEF");
   printf("I'm the child\n");
   printf("CPID: %d\n", getpid());
  }
